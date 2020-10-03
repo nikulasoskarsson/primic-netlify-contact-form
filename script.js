cecf89f50523ad84615a86ef09c5f6f28d266896
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-03 14:40:39 +0200

    switched to v2 of the prismic api

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 const headingEl = document.getElementById('heading');
+const prismicURL = 'https://netlify-test.cdn.prismic.io/api/v2';
 
 async function getHeading() {
   const txtFile = await fetch('apiKey.txt');
   const apiKey = await txtFile.text();
 
-  let conn = await fetch(
-    `https://netlify-test.prismic.io/api/v1?access_token=${apiKey}`
-  );
+  let conn = await fetch(`${prismicURL}?access_token=${apiKey}`);
   let res = await conn.json();
 
   const masterRef = res.refs[0].ref;
   conn = await fetch(
-    `https://netlify-test.prismic.io/api/v1/documents/search?ref=${masterRef}&access_token=${apiKey}`
+    `${prismicURL}/documents/search?ref=${masterRef}&access_token=${apiKey}`
   );
   res = await conn.json();
   console.log(res);
-  return res.results[0].data.heading.heading.value[0].text;
+  return res.results[0].data.heading[0].text;
 }
 
 async function insertDataFromPrismic() {
