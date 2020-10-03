1275330581a89d3cad219270e417afe2bc16ffb6
nikulasoskarsson <nikulasoskarsson@gmail.com>
2020-10-03 13:03:37 +0200

    uppdated getHeading to first fetch and then use the masterRef

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ const prismicURL = 'https://netlify-test.cdn.prismic.io/api/v2';
 var apiToken = 'MC5XVHFod1NrQUFDWUE0RVFw...e-_vX5dYu-_vQnvv70';
 
 async function getHeading() {
-  const conn = await fetch(
-    `https://netlify-test.prismic.io/api/v1/documents/search?ref=X3hMwhEAACYA4d1k#format=html`
-  );
-  const res = await conn.json();
+  let conn = await fetch(`https://netlify-test.prismic.io/api/v1`);
+  let res = await conn.json();
 
+  const masterRef = res.refs[0].ref;
+  conn = await fetch(
+    `https://netlify-test.prismic.io/api/v1/documents/search?ref=${masterRef}#format=html`
+  );
+  res = await conn.json();
+  console.log(res);
   return res.results[0].data.heading.heading.value[0].text;
 }
 
